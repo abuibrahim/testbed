e105fc15389d2c8f37b4a37453d049390431deb0
Ruslan Babayev <ruslan@babayev.com>
2022-07-03 13:09:01 -0700

    reset NVRAM from template when starting domains

--- Main.hs
+++ Main.hs
@@ -97,7 +97,7 @@ createDomains conn doc = do
   domains <- runX $ doc >>> atTag "domain" >>> (getNameText &&& writeString)
   forM_ domains $ \(name,xml) -> do
     putStrLn $ "Creating domain " ++ name
-    createDomainXML conn xml []
+    createDomainXML conn xml [ DomainStartResetNvram ]
 
 destroyTestbed xml = do
   let doc = readDocument [withValidate yes] xml
